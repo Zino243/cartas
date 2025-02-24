8145a4c9c36116d5ac996d90b90435a4351223ae
Zino243 <bloodxs86@gmail.com>
2025-02-24 19:30:49 +0000

    V0.1 [ == ] TERMINADO [ + ] Terminado el seguimiento del raton con un lerp [ + ] Agregado los sprites temporales para simular el mouse
    [ == ] FALTA!
    [-] Cada cierto tiempo actualizar la posicion real del sprite con la del raton (se va creando un offset)
    [-] Implementacion de mouse y personaje, TRHOW

--- scripts/control.gd
+++ scripts/control.gd
@@ -1,11 +1,9 @@
 extends Node2D
 
+@export var speed: float = 300.0  # Velocidad del seguimiento
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	position = get_global_mouse_position()
+func _process(delta):
+	var parent = get_parent()  # Obtener el nodo padre
+	if parent:
+		var target_pos = get_global_mouse_position()
+		parent.position = position.lerp(target_pos, 1 - exp(-speed * delta))  # Movimiento suave
